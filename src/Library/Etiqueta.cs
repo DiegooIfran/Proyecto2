759ca9e01f275bb1bf831f1a926609ae96da3a6e
paulinasilveira <paulina.silveira@correo.ucu.edu.uy>
2025-11-04 19:31:29 -0300

    Arreglos clase Etiqueta

--- src/Library/Etiqueta.cs
+++ src/Library/Etiqueta.cs
@@ -1,13 +1,30 @@
 namespace Library;
 
+// Representa una etiqueta que puede asociarse a un cliente
 public class Etiqueta
 {
-    public string Nombre;
-    public string Descripcion;
-
+    public string Nombre { get; private set; }
+    public string Descripcion { get; private set; }
+    
+    //Crea una nueva etiqueta con su nombre y descripción
     public Etiqueta(string nombre, string descripcion)
     {
         Nombre = nombre;
         Descripcion = descripcion;
+    }
+    
+    //Asocia una etiqueta a un cliente
+    public static void AgregarEtiqueta(Cliente cliente, Etiqueta etiqueta)
+    {
+        cliente.ObtenerEtiquetas().Add(etiqueta);
+    }
+    
+    // Elimina una etiqueta de un cliente si ya la tiene
+    public static void BorrarEtiqueta(Cliente cliente, Etiqueta etiqueta)
+    {
+        if (cliente.ObtenerEtiquetas().Contains(etiqueta))
+        {
+            cliente.ObtenerEtiquetas().Remove(etiqueta);
+        }
     }
 }
