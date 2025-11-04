8f8dd32243fdd935c5470b3c2c7c4ea4453a59a6
SimoneQuinteros <simone.quinteros@correo.ucu.edu.uy>
2025-11-04 19:56:52 -0300

    Cambios en Administrador

--- src/Library/Administrador.cs
+++ src/Library/Administrador.cs
@@ -2,38 +2,25 @@ namespace Library;
 
 public class Administrador : Usuario
 {
-    private List<Vendedor> Vendedores { get; set; }
-    
-    public Administrador(string nombre, string apellido, string telefono, string email) 
+   public Administrador(string nombre, string apellido, string telefono, string email) 
         : base(nombre, apellido, telefono, email)
-    {
-        this.Vendedores = new List<Vendedor>();
-    }
+    { }
     
     public void CrearVendedor(string nombre, string apellido, string telefono, string email) 
     {
         Vendedor nuevoVendedor = new Vendedor(nombre, apellido, telefono, email);
-        Vendedores.Add(nuevoVendedor); 
     }
 
-    public void EliminarVendedor(string email) //Decido usar los Email ya que deberia ser unico
+    public void EliminarVendedor(Vendedor vendedor) //Decido usar los Email ya que deberia ser unico
     {
-        Vendedores.RemoveAll(v => v.ObtenerEmail() == email);
+        GestorVendedor.EliminarVendedor(vendedor);
     }
 
-    public void SuspenderVendedor(string email)
+    public void SuspenderVendedor(Vendedor vendedor)
     {
-        foreach (Vendedor vendedor in Vendedores)
-        {
-            if (vendedor.ObtenerEmail() == email)
-            {
-                vendedor.Activo = false;
-            }
-        } 
-    }
-
-    public List<Vendedor> ObtenerVendedores()
-    {
-        return this.Vendedores;
+         if (vendedor.Activo==true)
+         { 
+             vendedor.Activo = false;
+         }
     }
 }
