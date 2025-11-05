ae7af5dde41a60dd2b5250c60416f7730cd07cfa
Diego <diego.ifran@correo.ucu.edu.uy>
2025-11-05 08:37:53 -0300

    comentarios

--- src/Library/GestorInteracciones.cs
+++ src/Library/GestorInteracciones.cs
@@ -2,41 +2,41 @@ namespace Library;
 
 public static class GestorInteracciones
 {
-    private static List<Interaccion> _todasInteracciones = new List<Interaccion>();
-    public static void NuevoMensaje(Cliente cliente, DateTime fecha, string tema, string notas, bool enviada)
+    private static List<Interaccion> _todasInteracciones = new List<Interaccion>(); //Creo una lista para ver todas las interacciones
+    public static void NuevoMensaje(Cliente cliente, DateTime fecha, string tema, string notas, bool enviada) //Creo un nuevo mensaje
     {
         Mensaje mensaje = new Mensaje(fecha, tema, notas, enviada);
         cliente.AgregarInteraccion(mensaje);
         _todasInteracciones.Add(mensaje);
     }
-    public static void NuevaLlamada(Cliente cliente, DateTime fecha, string tema, string notas, bool enviada)
+    public static void NuevaLlamada(Cliente cliente, DateTime fecha, string tema, string notas, bool enviada) //Creo una nueva llamada
     {
         Llamadas llamada = new Llamadas(fecha, tema, notas, enviada);
         cliente.AgregarInteraccion(llamada);
         _todasInteracciones.Add(llamada);
     }
-    public static void NuevoCorreo(Cliente cliente, DateTime fecha, string tema, string notas, bool enviada)
+    public static void NuevoCorreo(Cliente cliente, DateTime fecha, string tema, string notas, bool enviada) //Creo un nuevo correo
     {
         Correo correo = new Correo(fecha, tema, notas, enviada);
         cliente.AgregarInteraccion(correo);
         _todasInteracciones.Add(correo);
     }
-    public static void NuevaReunion(Cliente cliente, DateTime fecha, string tema, string notas)
+    public static void NuevaReunion(Cliente cliente, DateTime fecha, string tema, string notas) //Creo una nueva reunion
     {
         Reunion reunion = new Reunion(fecha, tema, notas);
         cliente.AgregarInteraccion(reunion);
         _todasInteracciones.Add(reunion);
     }
 
-    public static List<Interaccion> UltimasInteracciones()
+    public static List<Interaccion> UltimasInteracciones() //Devuelvo una lista de las ultimas interacciones
     {
         return _todasInteracciones
-            .OrderByDescending(i => i.Fecha)
-            .Take(5)
+            .OrderByDescending(i => i.Fecha) //Ordena por fecha
+            .Take(5) //Toma 5
             .ToList();
     }
 
-    public static void VerInteracciones(Cliente cliente)
+    public static void VerInteracciones(Cliente cliente) //Imprimo todas las interacciones
     {
         foreach (Interaccion interaccion in cliente.ObtenerInteracciones())
         {
@@ -44,7 +44,7 @@ public static class GestorInteracciones
         }
     }
 
-    public static List<Online> InteraccionesPendientes()
+    public static List<Online> InteraccionesPendientes() //Devuelvo una lista de interacciones pendientes
     {
         return _todasInteracciones
             .OfType<Online>() // Solo queremos las de tipo Mensaje, Correo, Llamada
