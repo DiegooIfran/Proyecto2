9b88ae685ae62182427ff89c9a000a8139b0f675
Diego <diego.ifran@correo.ucu.edu.uy>
2025-11-05 08:41:06 -0300

    comentarios gestorcliente

--- src/Library/GestorCliente.cs
+++ src/Library/GestorCliente.cs
@@ -2,21 +2,21 @@ namespace Library;
 
 public class GestorCliente
 {
-    public List<Cliente> TotalClientes { get; set; }
+    public List<Cliente> TotalClientes { get; set; } //Lista de todos los clientes
 
-    public GestorCliente()
+    public GestorCliente() //Constructor del gestor
     {
         this.TotalClientes = new List<Cliente>();
     }
 
     public void AgregarCliente(string name, string apellido, string telefono, string email, string genero,
-        DateTime fechaNacimiento)
+        DateTime fechaNacimiento) //Creo un cliente
     {
         Cliente cliente = new Cliente(name, apellido, telefono, email, genero, fechaNacimiento);
         this.TotalClientes.Add(cliente);
     }
 
-    public void EliminarCliente(string email)
+    public void EliminarCliente(string email) //Elimino un cliente
     {
         foreach (Cliente cliente in this.TotalClientes.ToList())
         {
@@ -27,37 +27,37 @@ public class GestorCliente
         }
     }
 
-    public List<Cliente> VerTotalClientes()
+    public List<Cliente> VerTotalClientes() //Devuelvo una lista con todos los clientes
     {
         return this.TotalClientes;
     }
 
-    public void AsignarCliente(Vendedor vendedor, Cliente cliente)
+    public void AsignarCliente(Vendedor vendedor, Cliente cliente) //Le asigno un cliente a otro vendedor
     {
         vendedor.AgregarCliente(cliente);
     }
 
-    public static void ModificarNombre(string nombre, Cliente cliente)
+    public static void ModificarNombre(string nombre, Cliente cliente) //Modifico el nombre de un cliente
     {
         cliente.CambiarNombre(nombre);
     }
 
-    public static void ModificarApellido(string apellido, Cliente cliente)
+    public static void ModificarApellido(string apellido, Cliente cliente) //Modifico el apellido de un cliente
     {
         cliente.CambiarApellido(apellido);
     }
 
-    public static void ModificarTelefono(string telefono, Cliente cliente)
+    public static void ModificarTelefono(string telefono, Cliente cliente) //Modifico el telefono de un cliente
     {
         cliente.CambiarTelefono(telefono);
     }
 
-    public static void ModificarEmail(string email, Cliente cliente)
+    public static void ModificarEmail(string email, Cliente cliente) //Modifico el email de un cliente
     {
         cliente.CambiarEmail(email);
     }
 
-    public static Cliente BuscarPorNombre(string nombre, List<Cliente> clientes)
+    public static Cliente BuscarPorNombre(string nombre, List<Cliente> clientes) //Busco un cliente por nombre
     {
         foreach (Cliente cliente in clientes)
         {
@@ -70,7 +70,7 @@ public class GestorCliente
         throw new InvalidOperationException("No se encontró ningún cliente con ese nombre.");
     }
 
-    public static Cliente BuscarPorApellido(string apellido, List<Cliente> clientes)
+    public static Cliente BuscarPorApellido(string apellido, List<Cliente> clientes) //Busco un cliente por apellido
     {
         foreach (Cliente cliente in clientes)
         {
@@ -83,7 +83,7 @@ public class GestorCliente
         throw new InvalidOperationException("No se encontró ningún cliente con ese apellido.");
     }
 
-    public static Cliente BuscarPorTelefono(string telefono, List<Cliente> clientes)
+    public static Cliente BuscarPorTelefono(string telefono, List<Cliente> clientes) //Busco un cliente por telefono
     {
         foreach (Cliente cliente in clientes)
         {
@@ -96,7 +96,7 @@ public class GestorCliente
         throw new InvalidOperationException("No se encontró ningún cliente con ese telefono.");
     }
 
-    public static Cliente BuscarPorEmail(string email, List<Cliente> clientes)
+    public static Cliente BuscarPorEmail(string email, List<Cliente> clientes) //Busco un cliente por email
     {
         foreach (Cliente cliente in clientes)
         {
