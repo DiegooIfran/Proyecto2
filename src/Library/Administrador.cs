3c3113f69e9fade7abb78c2d3f8820e6963a7962
SimoneQuinteros <simone.quinteros@correo.ucu.edu.uy>
2025-11-05 08:44:57 -0300

    Correción en Administrador

--- src/Library/Administrador.cs
+++ src/Library/Administrador.cs
@@ -9,18 +9,28 @@ public class Administrador : Usuario
     public void CrearVendedor(string nombre, string apellido, string telefono, string email) 
     {
         Vendedor nuevoVendedor = new Vendedor(nombre, apellido, telefono, email);
+        GestorVendedor.AgregarVendedor(nuevoVendedor); //
     }
 
-    public void EliminarVendedor(Vendedor vendedor) //Decido usar los Email ya que deberia ser unico
-    {
-        GestorVendedor.EliminarVendedor(vendedor);
+    public void EliminarVendedor(string email) //Decido usar los Email ya que deberia ser unico
+    { 
+        foreach (var vendedor in GestorVendedor.VerTotalVendedores())
+        {
+            if (vendedor.ObtenerEmail()== email)
+            {
+                GestorVendedor.EliminarVendedor(vendedor);
+            }
+        }
     }
 
-    public void SuspenderVendedor(Vendedor vendedor)
+    public void SuspenderVendedor(string email)
     {
-         if (vendedor.Activo==true)
-         { 
-             vendedor.Activo = false;
-         }
+        foreach (var vendedor in GestorVendedor.VerTotalVendedores())
+        {
+            if (vendedor.ObtenerEmail()== email)
+            { 
+                vendedor.Activo = false;
+            }
+        }
     }
 }
