fdef52579e50a48785ae3dac7a2aa9aa221c51d8
SimoneQuinteros <simone.quinteros@correo.ucu.edu.uy>
2025-11-05 08:57:31 -0300

    Correción y comentario  en Administrador

--- src/Library/Administrador.cs
+++ src/Library/Administrador.cs
@@ -9,25 +9,25 @@ public class Administrador : Usuario
     public void CrearVendedor(string nombre, string apellido, string telefono, string email) 
     {
         Vendedor nuevoVendedor = new Vendedor(nombre, apellido, telefono, email);
-        GestorVendedor.AgregarVendedor(nuevoVendedor); //
+        GestorVendedor.AgregarVendedor(nuevoVendedor); //Lama al Singleton GestorVendedores para agregar el nuevo vendedor a la lista
     }
 
-    public void EliminarVendedor(string email) //Decido usar los Email ya que deberia ser unico
+    public void EliminarVendedor(string email) // Se usa el email porque es único para cada vendedor
     { 
-        foreach (var vendedor in GestorVendedor.VerTotalVendedores())
+        foreach (var vendedor in GestorVendedor.VerTotalVendedores())//Recorre los la lista de vendedores
         {
-            if (vendedor.ObtenerEmail()== email)
+            if (vendedor.ObtenerEmail()== email)//Cuando el email del vendedor coincide con la string que se pasó se elimina el vendedor
             {
                 GestorVendedor.EliminarVendedor(vendedor);
             }
         }
     }
 
-    public void SuspenderVendedor(string email)
+    public void SuspenderVendedor(string email) 
     {
-        foreach (var vendedor in GestorVendedor.VerTotalVendedores())
+        foreach (var vendedor in GestorVendedor.VerTotalVendedores()) //Recorre los la lista de vendedores
         {
-            if (vendedor.ObtenerEmail()== email)
+            if (vendedor.ObtenerEmail()== email) //Cuando el email del vendedor coincide con la string que se pasó se cambia el estado del vendedor
             { 
                 vendedor.Activo = false;
             }
