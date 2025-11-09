bdd1f75ddd1a9c007d0df384c309543b8e235cb6
Luutyy <lautaramirezlopez@gmail.com>
2025-11-09 15:22:09 -0300

    Agrego gestor y singleton

--- src/Library/Gestor.cs
+++ src/Library/Gestor.cs
@@ -1,6 +1,37 @@
 namespace Library;
 
-public class Gestor
+public class Gestor<T> where T : Usuario 
 {
+    private List<T> _total = new List<T>(); //Hago la lista que contendra administradores o vendedores 
     
+    public void Agregar(T usuario)
+    {
+        _total.Add(usuario);
+    }
+
+    public List<T> VerTotal()
+    {
+        return _total;
+    }
+
+    public void Eliminar(T usuario)
+    {
+        _total.Remove(usuario);
+    }
+
+    public bool YaRegistrado(string email)
+    {
+        foreach (T usuario in _total)
+        {
+            if (usuario.ObtenerEmail() == email)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
+
+// Deberia haber solo 1 GestorAdministrador y 1 GestorVendedor por ende empleamos Singleton para hacer que solamente 
+// pueda existir 1 instancia de estos y ella contenga la lista y las funciones que deberia hacer
--- src/Library/Singleton.cs
+++ src/Library/Singleton.cs
@@ -1,6 +1,22 @@
 namespace Library;
 
-public class Singleton
+public class Singleton<T> where T : class, new()
 {
+    private static T _instance;
+    private Singleton()
+    {
+    }
+    
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new T();
+            }
+            return _instance;
+        }
+    }
     
 }
