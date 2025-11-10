b8e6672a0cffbd68dffc517f1887507877b56a9e
Diego <diego.ifran@correo.ucu.edu.uy>
2025-11-10 09:31:36 -0300

    Test Fachada

--- test/LibraryTests/TestFachada.cs
+++ test/LibraryTests/TestFachada.cs
@@ -18,7 +18,7 @@ public class TestFachada
         fachada.AgregarCliente("Juan", "Pérez", "099111111", email, "M", new DateTime(1990, 1, 1));
 
         // Act
-        var cliente = fachada.BuscarCliente(email);
+        var cliente = fachada.BuscarEmail(email);
 
         // Assert
         Assert.That(cliente, Is.Not.Null);
@@ -27,18 +27,58 @@ public class TestFachada
     }
     
     [Test]
-    public void SuspenderVendedores() // Suspendo a un vendedor
+    public void ModificarNombre_DeberiaActualizarElNombreDelCliente()
     {
-        Administrador admin = new Administrador("Lautaro", "Ramirez", "092773311", "lautaro.ramirez@gmail.com");
-        admin.CrearVendedor("Federico", "Garcia", "231231", "fedegarcia@gmail.com");
-        admin.SuspenderVendedor("fedegarcia@gmail.com");
-        Gestor<Vendedor> gestor = Singleton<Gestor<Vendedor>>.Instance;
-        Assert.That(gestor.VerTotal()[0].Activo, Is.EqualTo(false));
+        // Arrange
+        string email = "test2@mail.com";
+        fachada.AgregarCliente("Ana", "Lopez", "099222222", email, "F", new DateTime(1995, 5, 5));
+
+        // Act
+        fachada.ModificarNombre(email, "Andrea");
+        var cliente = fachada.BuscarEmail(email);
+
+        // Assert
+        Assert.That(cliente.ObtenerNombre(), Is.EqualTo("Andrea"));
     }
 
     [Test]
-    public void a()
+    public void ModificarApellido_DeberiaActualizarElApellidoDelCliente()
     {
-        
+        // Arrange
+        string email = "test3@mail.com";
+        fachada.AgregarCliente("Mario", "Gomez", "099333333", email, "M", new DateTime(1992, 3, 3));
+
+        // Act
+        fachada.ModificarApellido(email, "González");
+        var cliente = fachada.BuscarEmail(email);
+
+        // Assert
+        Assert.That(cliente.ObtenerApellido(), Is.EqualTo("González"));
+    }
+    
+    [Test]
+    public void EliminarCliente_DeberiaQuitarloDeLaLista()
+    {
+        // Arrange
+        string email = "test4@mail.com";
+        fachada.AgregarCliente("Carla", "Diaz", "099444444", email, "F", new DateTime(1998, 8, 8));
+
+        // Act
+        fachada.EliminarCliente(email);
+        var cliente = fachada.BuscarEmail(email);
+
+        // Assert
+        Assert.That(cliente, Is.Null);
+    }
+    
+    [Test]
+    public void Singleton_DeberiaDevolverLaMismaInstancia()
+    {
+        // Act
+        var instancia1 = Fachada.Instance;
+        var instancia2 = Fachada.Instance;
+
+        // Assert
+        Assert.That(instancia1, Is.SameAs(instancia2));
     }
 }
