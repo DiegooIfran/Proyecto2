5f44627de24863c5d871ddbd42e98b4c55641b99
Diego <diego.ifran@correo.ucu.edu.uy>
2025-11-10 09:21:51 -0300

    testfachada

--- test/LibraryTests/TestFachada.cs
+++ test/LibraryTests/TestFachada.cs
@@ -2,21 +2,28 @@ namespace Library.Tests;
 
 public class TestFachada
 {
+    private Fachada fachada;
+    
     [SetUp]
     public void Setup()
     {
-        Singleton<Gestor<Administrador>>.Instance.VerTotal().Clear();
-        Singleton<Gestor<Vendedor>>.Instance.VerTotal().Clear();
+        fachada = Fachada.Instance;
     }
     
     [Test]
-    public void CrearVendedores() // Que un admin cree un vendedor y lo almacene
+    public void AgregarYBuscarCliente_DeberiaDevolverElMismoCliente()
     {
-        Gestor<Vendedor> gestor = Singleton<Gestor<Vendedor>>.Instance;
-        Administrador admin = new Administrador("Lautaro", "Ramirez", "092773311", "lautaro.ramirez@gmail.com");
-        admin.CrearVendedor("Federico", "Garcia", "231231", "fedegarcia@gmail.com");
-        Assert.AreEqual(1, gestor.VerTotal().Count);
-        Assert.AreEqual("Federico", gestor.VerTotal()[0].ObtenerNombre());
+        // Arrange
+        string email = "test1@mail.com";
+        fachada.AgregarCliente("Juan", "Pérez", "099111111", email, "M", new DateTime(1990, 1, 1));
+
+        // Act
+        var cliente = fachada.BuscarCliente(email);
+
+        // Assert
+        Assert.That(cliente, Is.Not.Null);
+        Assert.That(cliente.ObtenerEmail(), Is.EqualTo(email));
+        Assert.That(cliente.ObtenerNombre(), Is.EqualTo("Juan"));
     }
     
     [Test]
@@ -28,14 +35,10 @@ public class TestFachada
         Gestor<Vendedor> gestor = Singleton<Gestor<Vendedor>>.Instance;
         Assert.That(gestor.VerTotal()[0].Activo, Is.EqualTo(false));
     }
-    
+
     [Test]
-    public void EliminarVendedores() // Elimino a un vendedor
+    public void a()
     {
-        Gestor<Vendedor> gestor = Singleton<Gestor<Vendedor>>.Instance;
-        Administrador admin = new Administrador("Lautaro", "Ramirez", "092773311", "lautaro.ramirez@gmail.com");
-        admin.CrearVendedor("Federico", "Garcia", "231231", "fedegarcia@gmail.com");
-        admin.EliminarVendedor("fedegarcia@gmail.com");
-        Assert.AreEqual(0, gestor.VerTotal().Count);
+        
     }
 }
