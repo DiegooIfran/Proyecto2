d3a1d6fd2e9053771f6b13d8ae53c0519893f5ad
Diego <diego.ifran@correo.ucu.edu.uy>
2025-12-01 18:35:32 -0300

    Tests de usuario cambio en nick

--- test/LibraryTests/TestUsuario.cs
+++ test/LibraryTests/TestUsuario.cs
@@ -10,21 +10,22 @@ public class TestUsuario
     [Test]
     public void Heredar() // Verifico que efectivamente hereden los atributos
     {
-        var vendedor = new Vendedor("Lautaro", "Ramirez", "092773311", "lautaro.ramirez@gmail.com", TODO);
+        var vendedor = new Vendedor("Lautaro", "Ramirez", "092773311", "lautaro.ramirez@gmail.com", ".luty");
 
         Assert.AreEqual("Lautaro", vendedor.ObtenerNombre());
         Assert.AreEqual("Ramirez", vendedor.ObtenerApellido());
         Assert.AreEqual("lautaro.ramirez@gmail.com", vendedor.ObtenerEmail());
         Assert.AreEqual("092773311", vendedor.ObtenerTelefono());
+        Assert.AreEqual(".luty", vendedor.ObtenerNick());
     }
 
     [Test]
     public void InstanciasDe() // Verifico que administrador y vendedor sean instancias de usuario
     {
-        Usuario administrador = new Administrador("Carlos", "Diaz", "3213138", "carlosdiaz@gmail.com", TODO);
+        Usuario administrador = new Administrador("Carlos", "Diaz", "3213138", "carlosdiaz@gmail.com", ".luty");
         Assert.IsInstanceOf<Usuario>(administrador);
         
-        Usuario vendedor= new Vendedor("Federico", "Garcia", "231231", "fedegarcia@gmail.com", TODO);
+        Usuario vendedor= new Vendedor("Federico", "Garcia", "231231", "fedegarcia@gmail.com", ".luty");
         Assert.IsInstanceOf<Usuario>(vendedor);
     }
 
