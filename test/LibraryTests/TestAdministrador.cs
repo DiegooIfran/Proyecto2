e674790571530bc5e7d6ac3c737c9216ad8ddeb8
Diego <diego.ifran@correo.ucu.edu.uy>
2025-12-01 19:34:16 -0300

    Test admin correccion

--- test/LibraryTests/TestAdministrador.cs
+++ test/LibraryTests/TestAdministrador.cs
@@ -13,23 +13,21 @@ public class TestAdministrador
     {
         Singleton<GestorAdministrador>.Instance.VerTotal().Clear();
         Singleton<GestorVendedor>.Instance.VerTotal().Clear();
-
     }
     
     [Test]
     public void CrearVendedores() // Que un admin cree un vendedor y lo almacene
     {
-        Gestor<Vendedor> gestor = Singleton<Gestor<Vendedor>>.Instance;
-        Administrador admin = new Administrador("Lautaro", "Ramirez", "092773311", "lautaro.ramirez@gmail.com", "lauta");
+        fachada.CrearAdministrador("Lautaro", "Ramirez", "092773311", "lautaro.ramirez@gmail.com", "lauta");
         fachada.CrearVendedor("Federico", "Garcia", "231231", "fedegarcia@gmail.com", "lauta");
-        Assert.AreEqual(1, gestor.VerTotal().Count);
-        Assert.AreEqual("Federico", gestor.VerTotal()[0].ObtenerNombre());
+        Assert.AreEqual(1, gv.VerTotal().Count);
+        Assert.AreEqual("Federico", gv.VerTotal()[0].ObtenerNombre());
     }
     
     [Test]
     public void SuspenderVendedores() // Suspendo a un vendedor
     {
-        Administrador admin = new Administrador("Lautaro", "Ramirez", "092773311", "lautaro.ramirez@gmail.com", "lauta");
+        fachada.CrearAdministrador("Lautaro", "Ramirez", "092773311", "lautaro.ramirez@gmail.com", "lauta");
         fachada.CrearVendedor("Federico", "Garcia", "231231", "fedegarcia@gmail.com", "lauta");
         fachada.SuspenderUsuario("fedegarcia@gmail.com");
         Assert.That(gv.VerTotal()[0].Activo, Is.EqualTo(false));
@@ -39,7 +37,7 @@ public class TestAdministrador
     public void EliminarVendedores() // Elimino a un vendedor
     {
         
-        Administrador admin = new Administrador("Lautaro", "Ramirez", "092773311", "lautaro.ramirez@gmail.com", "lauta");
+        fachada.CrearAdministrador("Lautaro", "Ramirez", "092773311", "lautaro.ramirez@gmail.com", "lauta");
         fachada.CrearVendedor("Federico", "Garcia", "231231", "fedegarcia@gmail.com", "lauta");
         fachada.EliminarVendedor("fedegarcia@gmail.com");
         Assert.AreEqual(0, gv.VerTotal().Count);
